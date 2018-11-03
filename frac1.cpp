1fc3d97bbee4c5f8726d0ed5fe63107b41c60609
trunghe <33113755+trunghe@users.noreply.github.com>
2018-11-03 14:56:59 +0700

    Update frac1.cpp

--- frac1.cpp
+++ frac1.cpp
@@ -17,60 +17,6 @@ Author: Vu Thanh Trung
 
 using namespace std;
 
-//typedef long long ll;
-//typedef vector<int> vi;
-//typedef vector<vi> vvi;
-//typedef vector<ll> vl;
-//typedef vector<vl> vvl;
-//typedef vector<vvl> vvvl;
-//typedef pair<int, int> ii;
-//typedef vector<ii> vii;
-//typedef vector<vii> vvii;
-
-//#define FOR(i, a, b) \
-//    for (int i = (a); i < (b); i++)
-//#define FORE(i, a, b) \
-//    for (int i = (a); i <= (b); i++)
-//#define FORD(i, a, b) \
-//    for (int i = (a); i >= (b); i--)
-
-//#define INF 1e9+7
-//#define INFLL 1e18+7
-//#define esp 1e-9
-//#define PI 3.14159265
-//
-//ll nMod = 1e9 + 7;
-
-//inline ll GCD(ll a, ll b) {while (b != 0) {ll c = a % b; a = b; b = c;} return a;};
-//inline ll LCM(ll a, ll b) {return (a / GCD(a,b)) * b;};
-
-//ll max(ll a1, ll b1) {
-//    return a1 > b1 ? a1 : b1;
-//}
-
-
-//#define li list<int>
-//// function to find the divisors in order
-//li getDivisors(int n) {
-//    // Lists to store halves of the divisors
-//    li firstHalf;
-//    li secondHalf;
-//    int sqrtN = sqrt(n);
-//    FORE(i, 1, sqrtN) {
-//        if (n % i == 0) {
-//            firstHalf.emplace_back(i);
-//            int nOverI = n / i;
-//            if (nOverI != i) // check if divisors are NOT equal
-//            {
-//                // push the second divisor in the second half
-//                secondHalf.emplace_front(nOverI);
-//            }
-//        }
-//    }
-//    firstHalf.merge(secondHalf);
-//    return firstHalf;
-//}
-
 struct fraction {
     int up, down;
 
